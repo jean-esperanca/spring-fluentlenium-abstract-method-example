097647c8215f236efa2e09fda46a6f51a63d3bb3
Jeff Dean <jeff@galvanize.com>
2016-12-20 13:09:12 -0700

    eliminated some dependencies to track down the bug

--- src/test/java/com/example/DuckDuckGoTest.java
+++ src/test/java/com/example/DuckDuckGoTest.java
@@ -10,10 +10,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class DuckDuckGoTest extends FluentTest {
     @Test
     public void title_of_duck_duck_go_should_contain_search_query_name() {
-        goTo("http://stackoverflow.com/");
-        $("[name=q]").fill().with("FluentLenium");
-        $("[name=q]").submit();
-        System.out.println(window().title());
+        goTo("https://duckduckgo.com");
+        $("#search_form_input_homepage").fill().with("FluentLenium");
+        $("#search_button_homepage").submit();
         assertThat(window().title()).contains("FluentLenium");
     }
 }
